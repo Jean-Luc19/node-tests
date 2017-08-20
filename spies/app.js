2e2b96631ae0e4a71885ba035019209138f30845
Aaron Gottlieb <atticus19@gmail.com>
2017-08-19 21:24:20 -0600

    adds spy test

--- spies/app.js
+++ spies/app.js
@@ -1,5 +1,5 @@
-const db = require('./db');
+let db = require("./db");
 module.exports.handleSignup = (email, password) => {
   //check unique email, save user to db, send welcome email
-  db.saveUser({ email, password })
+  db.saveUser({ email, password });
 };
--- spies/app.test.js
+++ spies/app.test.js
@@ -1,8 +1,24 @@
-const expect = require('expect');
+const expect = require("expect");
+const rewire = require("rewire");
 
-describe('App', () => {
-  it('should call the spy correctly', () => {
-    const spy = expect.createSpy();
-    spy();
-  })
-})
+const app = rewire("./app");
+
+describe("App", () => {
+  let db = {
+    saveUser: expect.createSpy()
+  };
+  app.__set__("db", db);
+
+  it("should call the spy correctly", () => {
+    let spy = expect.createSpy();
+    spy("Shane", 25);
+    expect(spy).toHaveBeenCalledWith("Shane", 25);
+  });
+
+  it("should call save user with user object", () => {
+    let email = "test@test.com";
+    let password = "testpassword";
+    app.handleSignup(email, password);
+    expect(db.saveUser).toHaveBeenCalledWith({ email, password });
+  });
+});
